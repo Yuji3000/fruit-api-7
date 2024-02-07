a7605179ed14b5b439aa448124b26182153738d7
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2024-02-07 16:10:58 -0700

    Add links on /fruits/nutrional page. changed post request for fruits to get, original setup was post, dont know why

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "welcome#index"
-  post "/fruits", to: "fruits#index"
+  get "/fruits", to: "fruits#index"
   get "/fruits/nutritional", to: "nutritional#index"
 end
